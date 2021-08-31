e86578005df266adb857ebb7e57e9416f151b3fe
JinA-Kim-hub <zooro09347@gmail.com>
2021-08-31 09:10:43 +0900

    0831

--- 02-if문/src/step01_1330_두수_비교하기/Main3.java
+++ 02-if문/src/step01_1330_두수_비교하기/Main3.java
@@ -1,6 +1,6 @@
 package step01_1330_두수_비교하기;
 import java.util.Scanner;
-//삼항 연산자로 풀기1
+//삼항 연산자로 풀기2
 
 /*
 첫째 줄에 다음 세 가지 중 하나를 출력한다.
@@ -9,9 +9,9 @@ A가 B보다 작은 경우에는 '<'를 출력한다.
 A와 B가 같은 경우에는 '=='를 출력한다.
 */
 
-/****************************
+
+/**********
   삼항 연산자 :  조건이 간단할 때 가독성을 위해 사용 >> 조건식이 복잡할 때는 if-else 사용 ㄱ
-  
   변수 = (조건문) ? (true 시 연산) : (false 시 연산) ;
   
   //중첩해서 응용도 가능!!!
@@ -19,19 +19,23 @@ A와 B가 같은 경우에는 '=='를 출력한다.
   변수 = (조건문) ? (true 시 연산) : ((조건문) ? (true 시 연산) : (false 시 연산)) ;
 	
 */
+/**
+ * 
+ * Main2에서 나아가서...
+ * String 변수 생성 없이 바로 println 에 넣는 식으로 
+ * 코드 정리하기..
+ *
+ */
 
 
-
-
-public class Main2 {
+public class Main3 {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		
 		int A = s.nextInt();
 		int B = s.nextInt();
 		
-		String C = (A>B) ? ">" : ((A==B) ? "==" : "<" );
-		System.out.println(C);
+		System.out.println((A>B) ? ">" : ((A==B) ? "==" : "<" ));
 		
 		s.close();
  
