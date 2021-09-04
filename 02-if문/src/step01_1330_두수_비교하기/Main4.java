a69f3b39c0db52cdc966ed187839bdced831bc0c
JinA-Kim-hub <zooro09347@gmail.com>
2021-09-05 01:23:28 +0900

    런타임에러 numberformat 추후 수정

--- 02-if문/src/step01_1330_두수_비교하기/Main4.java
+++ 02-if문/src/step01_1330_두수_비교하기/Main4.java
@@ -1,6 +1,11 @@
 package step01_1330_두수_비교하기;
 //BufferedReader 사용해서 풀어보기
+//런타임 에러 numberformat _ 0905
 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /*
 첫째 줄에 다음 세 가지 중 하나를 출력한다.
@@ -13,9 +18,32 @@ A와 B가 같은 경우에는 '=='를 출력한다.
 
 public class Main4 {
 	public static void main(String[] args) {
+		/*
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String A = null,B = null;
+		int C,D = 0;
 		
+		try {
+			A=br.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			B=br.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
+		C = Integer.parseInt(A);
+		D = Integer.parseInt(B);
 		
+		System.out.println((C>D) ? ">" : ((C==D) ? "==" : "<"));
+		try {
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		*/
 	}
 
 }
