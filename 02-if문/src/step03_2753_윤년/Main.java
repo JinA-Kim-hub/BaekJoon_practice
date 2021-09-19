42273b075bdfa53be6ca5fadfd9da1db77f45626
JinA-Kim-hub <zooro09347@gmail.com>
2021-09-19 23:30:58 +0900

    틀렸음 수정 필요

---
+++ 02-if문/src/step03_2753_윤년/Main.java
@@ -0,0 +1,27 @@
+package step03_2753_윤년;
+
+import java.util.Scanner;
+
+//윤년
+
+// 문제 출처))
+// https://www.acmicpc.net/problem/2753
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		int year = s.nextInt();
+		
+		if((year%4==0 && year%100 != 0) || (year%400 !=0))
+			System.out.print(1);
+		else
+			System.out.print(0);
+		
+		
+		
+		s.close();
+		
+	}
+
+}
