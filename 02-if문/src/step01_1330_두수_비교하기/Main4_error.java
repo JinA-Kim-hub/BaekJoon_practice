172e625117cf1e06b0ce55b48a192928c5e2fd85
JinA-Kim-hub <zooro09347@gmail.com>
2021-09-19 22:04:48 +0900

    런타임 에러 공백제거를 해야 하는데 안해서 발생, 문제 출처 표기

--- 02-if문/src/step01_1330_두수_비교하기/Main4_error.java
+++ 02-if문/src/step01_1330_두수_비교하기/Main4_error.java
@@ -1,6 +1,9 @@
 package step01_1330_두수_비교하기;
 //BufferedReader 사용해서 풀어보기
 
+//문제출처 ))
+//https://www.acmicpc.net/problem/1330
+
 /*
 
 런타임 에러 numberformat _ 0905
@@ -19,14 +22,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/*
-첫째 줄에 A와 B가 주어진다. A와 B는 [공백 한 칸]으로 구분되어져 있다.
-첫째 줄에 다음 세 가지 중 하나를 출력한다.
-A가 B보다 큰 경우에는 '>'를 출력한다.
-A가 B보다 작은 경우에는 '<'를 출력한다.
-A와 B가 같은 경우에는 '=='를 출력한다.
 
-*/
 
 
 public class Main4_error {
