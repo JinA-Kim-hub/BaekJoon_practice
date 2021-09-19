71f67237df6acc1db23932421250f986d3cf371a
JinA-Kim-hub <zooro09347@gmail.com>
2021-09-19 22:10:18 +0900

    BufferedReader, ternary operator 문제 출처 표기

--- 02-if문/src/step02_9498_시험_성적/Main3.java
+++ 02-if문/src/step02_9498_시험_성적/Main3.java
@@ -1,5 +1,6 @@
 package step02_9498_시험_성적;
-
+//문제출처))
+//https://www.acmicpc.net/problem/9498
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,19 +8,6 @@ import java.io.InputStreamReader;
 //BufferedReader 사용 -> Scanner보다 좀더 빠름
 //삼항연산자(ternary operator)
 
-
-
-/*
-
- 시험 점수를 입력받아 90 ~ 100점은 A, 
- 80 ~ 89점은 B, 70 ~ 79점은 C,
- 60 ~ 69점은 D, 
- 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
- 
- 첫째 줄에 시험 점수가 주어진다. 
- 시험 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.  =>데이터 검증(validation) 필요!
-
-*/
 
 public class Main3 {
 	public static void main(String[] args) {
