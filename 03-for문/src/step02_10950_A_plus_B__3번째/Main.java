10e896754dbc6677eccc3d5f4ce8edb37712a357
JinA-Kim-hub <zooro09347@gmail.com>
2021-09-26 23:04:37 +0900

    좀더 생각 필요

--- 03-for문/src/step02_10950_A_plus_B__3번째/Main.java
+++ 03-for문/src/step02_10950_A_plus_B__3번째/Main.java
@@ -1,8 +1,11 @@
 package step02_10950_A_plus_B__3번째;
+// 문제 출처
+// https://www.acmicpc.net/problem/10950
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,10 +14,12 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str = null;
 		String str2 = null;
+		ArrayList<String> arr = new ArrayList<String>();
 		
 		try {
 			str = br.readLine();
 			str2 = br.readLine();
+			arr.add(str2);
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
@@ -27,9 +32,12 @@ public class Main {
 		}
 		
 		int T = Integer.parseInt(str);
+		for(int i = 0; i < T ; i++ ) {
 		StringTokenizer st = new StringTokenizer(str2);
-		
-		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		System.out.println(a+b);
+		}
 		
 	}
 }
