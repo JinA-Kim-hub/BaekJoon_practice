07bd6e4de03d098c889c95f1f83e321bd19ffd89
JinA-Kim-hub <zooro09347@gmail.com>
2021-10-02 23:44:56 +0900

    BufferedReader, StringTokenizer, ArrayList

--- 03-for문/src/step02_10950_A_plus_B__3번째/Main.java
+++ 03-for문/src/step02_10950_A_plus_B__3번째/Main.java
@@ -7,19 +7,31 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
+/*
+나의 풀이))
+맨 처음 입력 받은 숫자 만큼 
+띄어쓰기를 구분자로 서로 더해줄 숫자 두개를 
+String으로 한줄 씩 입력 받는다.
 
+StringTokenizer로 띄어쓰기를 제외한 숫자 2개를 뽑아낸다.
+그 두개의 숫자를 더해서 출력
+
+*/
 public class Main {
 	public static void main(String[] args) {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = null;
-		String str2 = null;
+		String str = null;//총 줄 개수
+		String str2 = null;//띄어쓰기로 구분하여 숫자 2개 입력
 		ArrayList<String> arr = new ArrayList<String>();
-		
+		int T = 0;
 		try {
 			str = br.readLine();
+			T = Integer.parseInt(str);//2개의 숫자를 입력 받을 줄 수를 입력
+			for(int i=0 ; i <T; i++) {//T만큼 숫자 2개를 한줄 씩 입력
 			str2 = br.readLine();
 			arr.add(str2);
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
@@ -31,13 +43,23 @@ public class Main {
 				}
 		}
 		
-		int T = Integer.parseInt(str);
+		
 		for(int i = 0; i < T ; i++ ) {
-		StringTokenizer st = new StringTokenizer(str2);
+		StringTokenizer st = new StringTokenizer(arr.get(i));
 		int a = Integer.parseInt(st.nextToken());
 		int b = Integer.parseInt(st.nextToken());
 		System.out.println(a+b);
 		}
+		
+		
+/*
+5
+1 1
+2 3
+3 4
+9 8
+5 2
+*/
 		
 	}
 }
