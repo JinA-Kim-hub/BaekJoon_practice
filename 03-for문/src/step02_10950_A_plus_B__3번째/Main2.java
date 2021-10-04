332fea96577f6740eaf2dcc40df0cd497fc650bc
JinA-Kim-hub <zooro09347@gmail.com>
2021-10-04 23:30:27 +0900

    Scanner, 개선된 for문

--- 03-for문/src/step02_10950_A_plus_B__3번째/Main2.java
+++ 03-for문/src/step02_10950_A_plus_B__3번째/Main2.java
@@ -1,28 +1,23 @@
 package step02_10950_A_plus_B__3번째;
-
 import java.util.Scanner;
-
-//문제 출처
-//https://www.acmicpc.net/problem/10950
-
-//Scanner
+ 
 public class Main2 {
-	public static void main(String[] args) {
+	public static void main(String args[]) {
+ 
 		Scanner s = new Scanner(System.in);
-		
+ 
 		int T = s.nextInt();
+		int[] sum = new int[T];
 		
-		String[] userChoice = null;
-		
-		for(int i=0;i<T;i++) {
-			s.nextLine();	//개행문자(엔터)를 제거하기 위해 추가****
-			userChoice = s.nextLine().split(" ");//split(String regex) <- 안에 구분자를 넣어서 문자열을 나눈다.
+		for(int i=0 ; i <T;i++) {
+			int a = s.nextInt();// nextInt()는 [공백 전까지]의 정수를 입력 받는다. **************
+			int b = s.nextInt();
+			sum[i] = a+b;
 		}
-		for(int i=0;i<T;i++) {
-			//System.out.println(Integer.parseInt(userChoice[i])+Integer.parseInt(userChoice[i+1]));
-			System.out.println(userChoice[i]);
+		for(int a : sum) {
+			System.out.println(a);
 		}
-		s.close();
+				s.close();
 	}
 }
 /*
