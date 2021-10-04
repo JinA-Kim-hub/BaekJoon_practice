ae6d5486762836d4a6130409f607727cf069637e
JinA-Kim-hub <zooro09347@gmail.com>
2021-10-05 00:32:57 +0900

    Scanner

---
+++ 03-for문/src/step03_8393_합/Main1.java
@@ -0,0 +1,21 @@
+package step03_8393_합;
+
+import java.util.Scanner;
+
+//문제 출처))
+//https://www.acmicpc.net/problem/8393
+
+public class Main1 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		int a = s.nextInt();
+		int sum = 0;
+		for(int i =1; i<= a; i++) {
+			sum += i;
+		}
+		System.out.println(sum);
+		
+		s.close();
+	}
+}
