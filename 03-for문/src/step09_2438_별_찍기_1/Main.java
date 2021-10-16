998bf775fafd147e0bc7dcb1848a2c2d4a0b5bcb
JinA-Kim-hub <zooro09347@gmail.com>
2021-10-16 23:46:10 +0900

    star_printer_01

---
+++ 03-for문/src/step09_2438_별_찍기_1/Main.java
@@ -0,0 +1,18 @@
+package step09_2438_별_찍기_1;
+
+import java.util.Scanner;
+
+//별찍기
+public class Main {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int N = s.nextInt();
+		for(int i = 1; i<=N ; i++) {
+			for(int j = 1; j<=i ; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		s.close();
+	}
+}
