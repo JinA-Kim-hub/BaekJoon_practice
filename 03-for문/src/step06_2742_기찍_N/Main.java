7bacd536f2cb899d0f440f43485bc9089acbb63a
JinA-Kim-hub <zooro09347@gmail.com>
2021-10-17 00:00:15 +0900

    Scanner

---
+++ 03-for문/src/step06_2742_기찍_N/Main.java
@@ -0,0 +1,14 @@
+package step06_2742_기찍_N;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int N = s.nextInt();
+		for(int i = N ; i>0 ; i--)
+			System.out.println(i);
+		
+		s.close();
+	}
+}
