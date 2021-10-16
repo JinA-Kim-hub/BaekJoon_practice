6708dd54d27eaba8849540f2b8b0a06cfbd91cf0
JinA-Kim-hub <zooro09347@gmail.com>
2021-10-17 02:02:06 +0900

    Scanner , ArrayList

---
+++ 03-for문/src/step11_10871_X보다_작은_수/Main.java
@@ -0,0 +1,36 @@
+package step11_10871_X보다_작은_수;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+
+// 복습 개념))
+// initialized :
+// 반복 사이클 수행 이전에 효율적인 시작 절차를 결정하기 위하여 필요한 예비 단계.
+// 지역변수 초기화를 안해줬을 경우 -> The local variable A may not have been initialized
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		
+		ArrayList<String> A = new ArrayList<String>(); //수열 A
+		int N = s.nextInt(); //정수 N개
+		int X = s.nextInt(); //정수 X
+		
+		for(int i = 0; i<N ; i++) {
+			A.add(s.next());
+			if(Integer.parseInt(A.get(i)) < X) {
+				System.out.print(A.get(i) + " ");
+			}
+		}
+		s.close();
+	}
+}
+/*
+
+10 5					1 4 2 3	
+1 10 4 9 2 3 8 5 7 6
+ 
+*/
