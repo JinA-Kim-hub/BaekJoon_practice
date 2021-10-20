02fce55f722a12e7eecb91f27f2219ff95da736b
JinA-Kim-hub <zooro09347@gmail.com>
2021-10-21 01:55:53 +0900

    Scanner, 무한루프 if문 안에 break

--- 04-while문/src/step01_10954_A_더하기_B/Main.java
+++ 04-while문/src/step01_10954_A_더하기_B/Main.java
@@ -1,25 +1,17 @@
 package step01_10954_A_더하기_B;
 
-import java.util.ArrayList;
 import java.util.Scanner;
-//수정 필요!
+//if문 안에 break문을 사용해보자
 public class Main {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		ArrayList<String> arr = new ArrayList<String>();
-		String a=null, b=null;
-		int sum = 0;
-		
-		while(a.equals("0") && b.equals("0")) {
-			a = s.next();
-			b = s.next();
-			sum = Integer.parseInt(a) + Integer.parseInt(b);
-			arr.add(String.valueOf(sum));
-		}
-		
-		
-		for(String str : arr) {
-			System.out.println(str);
+		int a , b=0;
+		while(true) {
+			a = s.nextInt();
+			b = s.nextInt();
+			if(a==0 && b==0)
+			break;
+			System.out.println(a+b);
 		}
 		
 		
