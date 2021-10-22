5f4e3695444768c260999744dc226771d16c438e
JinA-Kim-hub <zooro09347@gmail.com>
2021-10-22 22:34:15 +0900

    Arrays.sort() 사용

---
+++ 05-1차원-배열/src/step01_10818_최소_최대/Main.java
@@ -0,0 +1,32 @@
+package step01_10818_최소_최대;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+//Arrays.sort(배열명) -> 오름차순 정렬
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+		int[] userNums = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		while (st.hasMoreTokens()) {
+			for (int i = 0; i < N; i++) {
+				userNums[i] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		Arrays.sort(userNums);
+		bw.write(userNums[0] + " " + userNums[N - 1]);
+		br.close();
+		bw.close();
+	}
+}
