1fc05cf3326a46868e684ac5b7c9a915196aa5b8
JinA-Kim-hub <zooro09347@gmail.com>
2021-10-24 23:38:41 +0900

    NoSuchElementException 에러 -> 수정 필요

--- 05-1차원-배열/src/step02_2562_최댓값/Main.java
+++ 05-1차원-배열/src/step02_2562_최댓값/Main.java
@@ -5,27 +5,36 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
+
+//java.util.NoSuchElementException -> 에러 해결 필요
+/*
+ 61Exception in thread "main" java.util.NoSuchElementException
+	at java.util.StringTokenizer.nextToken(Unknown Source)
+	at step02_2562_최댓값.Main.main(Main.java:18)
+ */
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int N = Integer.parseInt(br.readLine());
-		int[] userNums = new int[N];
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		int index = 0;
-
-		while (st.hasMoreTokens()) {
-			userNums[index] = Integer.parseInt(st.nextToken());
-			index++;
+		
+		int[] userNums = {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()),
+				Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()),
+				Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken())};
+		int count=0;
+		int max=0;
+		for(int i=0 ; i < userNums.length ; i++) {
+			
+			if(userNums[i]>max) {
+				max=userNums[i];
+				count++;
+			}
 		}
 
-		Arrays.sort(userNums);
-		bw.write(userNums[N-1] + "\n" + (userNums.length-1));
+		bw.write(userNums[count] + "\n" + count);
 		br.close();
 		bw.close();
 	}
