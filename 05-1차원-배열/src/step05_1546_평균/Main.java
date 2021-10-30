7e4ec104c85f00cee1233675214ebb9ee31fa80b
JinA-Kim-hub <zooro09347@gmail.com>
2021-10-30 23:40:12 +0900

    Scanner, Arrays

--- 05-1차원-배열/src/step05_1546_평균/Main.java
+++ 05-1차원-배열/src/step05_1546_평균/Main.java
@@ -2,28 +2,37 @@ package step05_1546_평균;
 
 import java.util.Arrays;
 import java.util.Scanner;
-
+//시험 정수 최댓값 M
+//모든 점수를 점수/M*100
 public class Main {
 	public static void main(String[] args) {
-		
 		Scanner s = new Scanner(System.in);
 		
-		double arr[] = new double[s.nextInt()];
 		
-		for(int i = 0; i < arr.length; i++) {
-			arr[i] = s.nextDouble();
+		int N = s.nextInt();//과목 수
+		double jumsu[] = new double[N];//
+		
+		for(int i = 0; i < N; i++) {
+			jumsu[i] = s.nextDouble();
 		}
 		
 		double sum = 0;
-		Arrays.sort(arr);
+		Arrays.sort(jumsu);
 		
-		for(int i = 0; i < arr.length; i++) {
-			sum += ((arr[i] / arr[arr.length-1]) * 100);
+		for(int i = 0; i < jumsu.length; i++) {
+			sum += ((jumsu[i] / jumsu[jumsu.length-1]) * 100);
 		}
-		System.out.print(sum / arr.length);
+		System.out.print(sum / jumsu.length);
+		
+		
 		
 		
 		s.close();
 		
 	}
 }
+
+/*
+ 9
+10 20 30 40 50 60 70 80 90
+ */
