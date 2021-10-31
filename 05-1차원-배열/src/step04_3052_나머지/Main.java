6c3913785422e86588ca2f8f5dca710de0e595a2
JinA-Kim-hub <zooro09347@gmail.com>
2021-10-31 19:34:14 +0900

    Scanner, HashSet 사용*****

--- 05-1차원-배열/src/step04_3052_나머지/Main.java
+++ 05-1차원-배열/src/step04_3052_나머지/Main.java
@@ -1,42 +1,42 @@
 package step04_3052_나머지;
 
+import java.util.HashSet;
 import java.util.Scanner;
 
 //자연수 10개 입력 받아서 42로 나누었을 때, 서로 다른 나머지가 몇 개 있는지 출력
+//HashSet 이용. -> 중복을 허용하지 않는다.
 public class Main {
 	public static void main(String[] args) {
 		
 		Scanner s = new Scanner(System.in);
+		HashSet<Integer> sh = new HashSet<Integer>();
 		
-		int[] arr = new int[10];
-		int[] arr2 = new int[10];
-		int a =0;
+		
+		int[] arr = {s.nextInt(),s.nextInt(),s.nextInt(),s.nextInt(),s.nextInt(),
+		                    s.nextInt(),s.nextInt(),s.nextInt(),s.nextInt(),s.nextInt()};
 		
 		for(int i =0; i < arr.length; i++) {
-			arr[i] = s.nextInt();
-			arr2[i] = arr[i]%42;
+			sh.add(arr[i]%42);
 		}
 		
-		//---------------------------------
-		
-		/*
-		  
-		  
-		  
-		Scanner in = new Scanner(System.in);
-		HashSet<Integer> h = new HashSet<Integer>();
- 
-		for (int i = 0; i < 10; i++) {
-			h.add(in.nextInt() % 42);
-		//입력받은 값의 나머지 값을 add메소드를 통해 HashSet에 저장
-		}
-        
-		in.close();
-		System.out.print(h.size());
-		 * 
-		 */
+		System.out.println(sh.size());
 		
 		
 		s.close();
 	}
 }
+/*
+
+ 1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+ -----------------------
+ 10
+ */
