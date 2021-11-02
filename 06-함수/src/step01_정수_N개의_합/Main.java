b88890ba8bb70bd8e4468392988275f344c69dc5
JinA-Kim-hub <zooro09347@gmail.com>
2021-11-02 23:25:36 +0900

    함수로만 구현

---
+++ 06-함수/src/step01_정수_N개의_합/Main.java
@@ -0,0 +1,20 @@
+package step01_정수_N개의_합;
+
+
+/*
+
+Scanner 없이 돌아가는 로직을 함수로만 구현
+
+*/
+public class Main {
+    long sum(int[] a) {
+        long ans = 0;
+        
+        for(int i=0;i<a.length;i++) {
+        	ans += a[i];
+        }
+        
+		return ans;
+    }
+
+}
