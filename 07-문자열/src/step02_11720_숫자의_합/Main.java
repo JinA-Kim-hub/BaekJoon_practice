005357d68853d5ef3b6e02e851b9b4d554cbdb40
JinA-Kim-hub <zooro09347@gmail.com>
2021-11-22 01:42:29 +0900

    Scanner, 아스키코드 복습

---
+++ 07-문자열/src/step02_11720_숫자의_합/Main.java
@@ -0,0 +1,27 @@
+package step02_11720_숫자의_합;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		
+		int N = s.nextInt();
+		int sum = 0;
+		s.nextLine();
+		
+		String str = s.nextLine();
+		
+		
+		for(int i =0; i < N; i++) {
+			
+			sum += str.charAt(i)-'0';
+			
+		}
+		
+		System.out.println(sum);
+		s.close();
+	}
+}
+
